cdd9a983e8bdd7ed6dd062eb1d66520505b8f109
sam-klok <sklok11@gmail.com>
2023-01-30 21:27:56 -0700

    Init

---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+Playing with Telerik Kendo for Angular. Fun.
+Thank you,
+Serge
+
+
+Based on:
+https://www.telerik.com/campaigns/kendo-ui/using-kendo-ui-with-angular-video-tutorial
+https://www.youtube.com/watch?v=wKnrjbhAzgE
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonControlPanelComponent } from './button-control-panel/button-control-panel.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonControlPanelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
