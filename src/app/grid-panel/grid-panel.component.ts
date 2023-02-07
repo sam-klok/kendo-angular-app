bfed37c0edb0feb9fbbdda0b27249f1460340bb3
sam-klok <sklok11@gmail.com>
2023-02-06 21:47:04 -0700

    adding filter by route param

--- src/app/grid-panel/grid-panel.component.ts
+++ src/app/grid-panel/grid-panel.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { customers } from '../customers';
+import {
+  CompositeFilterDescriptor,
+  FilterDescriptor,
+  process,
+} from '@progress/kendo-data-query';
+
 
 @Component({
   selector: 'app-grid-panel',
@@ -9,12 +15,56 @@ import { customers } from '../customers';
 })
 export class GridPanelComponent implements OnInit {
   public gridData: any[] = customers;
-  paramCompanyName: string | null = null;
+  paramContactName: string | null = null;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.paramCompanyName = this.route.snapshot.paramMap.get('CompanyName');
+    this.paramContactName = this.route.snapshot.paramMap.get('CompanyName');
+    console.log(`param received: ${this.paramContactName}`);
+
+    if (this.paramContactName)
+      this.onFilter(this.paramContactName);
+
+  }
+
+  public filter: CompositeFilterDescriptor = {
+    logic: 'and',
+    filters: [
+      {
+        field: 'ContactName',
+        operator: 'contains',
+        value: 'test',
+        ignoreCase: true,
+      },
+    ],
+  };
+
+  public onFilter(inputValue: string): void {
+    console.log(inputValue);
+    this.gridData = process(this.gridData, {
+      filter: {
+        logic: 'or',
+        filters: [
+          {
+            field: 'ContactName',
+            operator: 'contains',
+            value: inputValue,
+          },
+        ],
+      },
+    }).data;
+    
+    this.filter = {
+      logic: 'or',
+      filters: [
+        {
+          field: 'ContactName',
+          operator: 'contains',
+          value: inputValue,
+        },
+      ],
+    };
   }
 
 }
